0f07414a8132ff2d3e801af9cb9886d915bfbc77
ishtian revee <ishtian11@gmail.com>
2018-11-02 20:07:53 +0600

    player jump done

---
+++ Assets/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // configurable fields
+    public float jumpForce;
+    public Rigidbody2D rigidBody;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+	    {
+            rigidBody.velocity = Vector2.up * jumpForce;
+	    }
+	}
+}

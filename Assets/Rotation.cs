3a3027acc73c55599d9ac93336dcbe085d4a808b
ishtian revee <ishtian11@gmail.com>
2018-11-02 21:19:56 +0600

    circle added with rotation

---
+++ Assets/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    // configurable fields
+    public float rotationSpeed;
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+	}
+}
